04fa319cd443e498c5cbbdbde0ec47b295c91932
rmflight <r.flight79@gmail.com>
2021-10-01 13:22:46 -0400

    first real network visualization generated

--- kea3_functions.R
+++ kea3_functions.R
@@ -1,6 +1,8 @@
 # functions used for reading in and processing data
 # for the KEA3 enrichment analysis.
-
+library(tidygraph)
+library(dplyr)
+library(ggraph)
 filter_hpa_expression = function(hpa_expression, tissue = NULL, cutoff = NULL, n_sd = 2){
   
   if (!is.null(tissue)) {
@@ -31,6 +33,15 @@ filter_hpa_expression = function(hpa_expression, tissue = NULL, cutoff = NULL, n
   
   return(tissue_isexpressed)
 }
+
+parse_gmt = function(gmt_file){
+  gmt = scan(gmt_file, what = "", sep = "\n", quiet = TRUE)
+  gmt_split = strsplit(gmt, "\t")
+  names(gmt_split) = purrr::map_chr(gmt_split, ~ gsub("_.*", "", .x[[1]]))
+  gmt_split = purrr::map(gmt_split, ~ .x[-1])
+  gmt_split
+}
+
 
 calculate_mode_sd = function(pos_expression){
   density_expression = stats::density(pos_expression)
@@ -68,6 +79,7 @@ create_kea3_networks = function(){
   ppi_names = purrr::map(ppi_data, ~ names(.x))
   all_ppi = unique(c(unlist(ppi_data), unlist(ppi_names)))
   
+  
   ks_matrix = matrix(0, nrow = length(all_ks), ncol = length(all_ks))
   rownames(ks_matrix) = colnames(ks_matrix) = all_ks
   for (iks in ks_data) {
@@ -77,6 +89,18 @@ create_kea3_networks = function(){
     }
   }
   
+  ks_df = purrr::map_dfr(colnames(ks_matrix), function(col_name){
+    tmp_col = ks_matrix[, col_name]
+    tmp_col = tmp_col[tmp_col > 0]
+    if (length(tmp_col) > 0) {
+      out_df = data.frame(from = col_name, to = names(tmp_col), weight = tmp_col)
+      return(out_df)
+    } else {
+      return(NULL)
+    }
+  })
+  ks_df$type = "kinase-substrate"
+  
   ppi_matrix = matrix(0, nrow = length(all_ppi), ncol = length(all_ppi))
   rownames(ppi_matrix) = colnames(ppi_matrix) = all_ppi
   for (ippi in ppi_data) {
@@ -85,16 +109,30 @@ create_kea3_networks = function(){
       ppi_matrix[ppi_int, ippi[[ppi_int]]] = ppi_matrix[ppi_int, ippi[[ppi_int]]] + 1
     }
   }
-  ks_graph = tidygraph::as_tbl_graph(ks_matrix, mode = "out")
-  ppi_graph = tidygraph::as_tbl_graph(ppi_matrix)
+  ppi_df = purrr::map_dfr(colnames(ppi_matrix), function(col_name){
+    tmp_col = ppi_matrix[, col_name]
+    tmp_col = tmp_col[tmp_col > 0]
+    if (length(tmp_col) > 0) {
+      out_df = data.frame(from = col_name, to = names(tmp_col), weight = tmp_col)
+      return(out_df)
+    } else {
+      return(NULL)
+    }
+  })
+  ppi_df2 = data.frame(from = ppi_df$to,
+                       to = ppi_df$from,
+                       weight = ppi_df$weight)
+  
+  ppi_df$type = "ppi"
+  ppi_df2$type = "ppi"
+  ppi_full = rbind(ppi_df, ppi_df2)
+  ks_graph = tidygraph::as_tbl_graph(ks_df)
+  ppi_graph = tidygraph::as_tbl_graph(ppi_df)
+  full_graph = tidygraph::graph_join(ks_graph, ppi_graph)
+  return(full_graph)
 }
 
-parse_gmt = function(gmt_file){
-  gmt = scan(gmt_file, what = "", sep = "\n", quiet = TRUE)
-  gmt_split = strsplit(gmt, "\t")
-  names(gmt_split) = purrr::map_chr(gmt_split, ~ gsub("_.*", "", .x[[1]]))
-  gmt_split = purrr::map(gmt_split, ~ .x[-1])
-  gmt_split
-}
 
-create_interaction = function()
+# kea3_networks = create_kea3_networks()
+# saveRDS(kea3_networks, file = here::here("kea3_datasets", "kea3_networks_2021-09-30.rds"))
+# rm(kea3_networks)
