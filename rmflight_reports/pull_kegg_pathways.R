c32573fd060a3790fae54605014ca69945152959
rmflight <r.flight79@gmail.com>
2021-11-05 10:42:50 -0400

    first go at graphing the KEGG graphs that were significant.
    Feels like there is too much, even after merging highly redundant things

--- rmflight_reports/pull_kegg_pathways.R
+++ rmflight_reports/pull_kegg_pathways.R
@@ -1,7 +1,14 @@
 library(KEGGREST)
 library(KEGGgraph)
+library(dplyr)
 pathways = c("mapk" = "hsa04010", "pi3k" = "hsa04151", "ras" = "hsa04014", "egfr" = "hsa01521")
 
 pathway_data = purrr::map(pathways, function(.x){
-  
+  kgml = keggGet(.x, "kgml")
+  graph_df = parseKGML2DataFrame(kgml) %>%
+    dplyr::mutate(from = gsub("hsa:", "", from),
+                  to = gsub("hsa:", "", to))
+  graph_df
 })
+
+saveRDS(pathway_data, file = here::here("rmflight_reports", "kegg_pathway_graphs.rds"))
